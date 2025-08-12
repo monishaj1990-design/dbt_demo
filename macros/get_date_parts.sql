c94357397fda20c24469b6f2191f297687aa5a18
Monisha Murugesan <monishaj1990@gmail.com>
2025-08-12 02:53:27 +0000

    added macro function

---
+++ macros/get_date_parts.sql
@@ -0,0 +1,13 @@
+{% get_date_parts(date_column) %}
+
+   (SELECT struct( 
+    {{ date_column }} as Original_date,
+    EXTRACT(YEAR FROM {{ date_column}}) as year,
+    EXTRACT(MONTH FROM {{ date_column }}) as month,
+    EXTRACT(DAY FROM {{ date_column }}) as day,
+    EXTRACT(DAYOFWEEK FROM {{ date_column}}) as day_of_week,
+    EXTRACT(DAYOFYEAR FROM {{ date_column }}) as day_of_year,
+    EXTRACT(WEEK FROM {{ date_column }}) as week,
+    EXTRACT(QUARTER FROM {{ date_column }}) as quarter)
+   )
+{% endmacro %}
